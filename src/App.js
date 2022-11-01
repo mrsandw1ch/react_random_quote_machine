86febd6b11f9fbd545325d6c0486f7926e65d408
Dmitry <dksviridenko@gmail.com>
2022-11-01 19:33:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import './App.css';
+import { quotes } from './quotes';
+import { colors } from './colors';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomQuoteNumber: Math.floor(Math.random() * (quotes.length)),
+      randomColorNumber: Math.floor(Math.random() * (colors.length)),
+      textFade: false
+    };
+    this.newNumber = this.newNumber.bind(this);
+  }
+
+  newNumber() {
+    let newRQN = Math.floor(Math.random() * (quotes.length));
+    let newRCN = Math.floor(Math.random() * (colors.length));
+    while (newRQN === this.state.randomQuoteNumber) {
+      newRQN = Math.floor(Math.random() * (quotes.length))
+    };
+    while (newRCN === this.state.randomColorNumber) {
+      newRCN = Math.floor(Math.random() * (colors.length))
+    };
+    this.setState({
+      textFade: true,
+      randomColorNumber: newRCN
+    })
+    setTimeout(() => {
+      this.setState({
+        randomQuoteNumber: newRQN
+      })
+    }, "500");
+    console.log(document.getElementById('quote-box').style)
+    setTimeout(() => {
+      this.setState({
+        textFade: false
+      })}, "1000");
+
+  }
+
+  render() {
+    const randomQuote = quotes[this.state.randomQuoteNumber].quote;
+    const randomQuoteAuthor = quotes[this.state.randomQuoteNumber].author;
+    const randomColor = colors[this.state.randomColorNumber];
+
+    return (
+      <div className="App" style={{backgroundColor: randomColor}}>
+        <div id='quote-box'>
+          <blockquote>
+            <p
+              id='quote'
+              style={{color: randomColor, animationName: (this.state.textFade ? 'text-fade' : 'none')}}>
+              <i className="fa-sharp fa-solid fa-quote-left quotation-sign"></i>
+              {randomQuote}
+            </p>
+          </blockquote>
+          <p
+            id='author'
+            style={{color: randomColor, animationName: (this.state.textFade ? 'text-fade' : 'none')}}>
+            - {randomQuoteAuthor}
+          </p>
+          <div id='buttons'>
+            <a
+              id='tweet-quote'
+              href="twitter.com/intent/tweet"
+              target="_blank"
+              style={{backgroundColor: randomColor}}>
+              <i className='fa-brands fa-twitter'></i>
+            </a>
+            <button
+              id='new-quote'
+              onClick={this.newNumber}
+              style={{backgroundColor: randomColor}}
+              disabled={this.state.textFade}>
+              New quote
+            </button>
+          </div>
+        </div>
+        <p style={{color: 'white', fontSize: '1rem'}}>by mrsandw1ch</p>
+      </div>
+    );
+  };
+}
+
+export default App;
---
+++ src/quotes.js
@@ -0,0 +1,54 @@
+export const quotes = [
+    {
+        quote: "You are the shuckiest shuck faced shuck in the world!",
+        author: "James Dashner, The Maze Runner"
+    },
+    {
+        quote: "I'm unpredictable, I never know where I'm going until I get there, I'm so random, I'm always growing, learning, changing, I'm never the same person twice. But one thing you can be sure of about me; is I will always do exactly what I want to do.",
+        author: "C. JoyBell C."
+    },
+    {
+        quote: "That proves you are unusual,\" returned the Scarecrow; \"and I am convinced that the only people worthy of consideration in this world are the unusual ones. For the common folks are like the leaves of a tree, and live and die unnoticed.",
+        author: "L. Frank Baum, The Land of Oz"
+    },
+    {
+        quote: "But that was life: Nobody got a guided tour to their own theme park. You had to hop on the rides as they presented themselves, never knowing whether you would like the one you were in line for...or if the bastard was going to make you throw up your corn dog and your cotton candy all over the place.",
+        author: "J.R. Ward, Crave"
+    },
+    {
+        quote: "Her name badge read: Hello! My name is DIE, DEMIGOD SCUM!",
+        author: "Rick Riordan, The Son of Neptune"
+    },
+    {
+        quote: "Insane means fewer cameras!",
+        author: "Ally Carter, Only the Good Spy Young"
+    },
+    {
+        quote: "A musician must make music, an artist must paint, a poet must write, if he is to be ultimately at peace with himself. What a man can be, he must be",
+        author: "Abraham Maslow"
+    },
+    {
+        quote: "I'm about as intimidating as a butterfly.",
+        author: "Dan Howell"
+    },
+    {
+        quote: "Flirtation doesn't have to go somewhere; it certainly doesn't need to end up in bed. I like to think of it as a little friendlier than a handshake, a little less intimate than a kiss. It's a way of saying hi, you look great, have a wonderful day. A tasteful flirtation, played out people who understand the rules, leave everyone feeling good and can perk up the bluest mood.",
+        author: "Karen Marie Moning, Bloodfever"
+    },
+    {
+        quote: "The lampshade on my head is for my bright ideas. I won't be able to convey them until Monday, when my curtain gets out of the dry cleaners.",
+        author: "Bauvard, Some Inspiration for the Overenthusiastic"
+    },
+    {
+        quote: "Never ask an elf for help; they might decide your better off dead, eh?",
+        author: "Christopher Paolini, Eldest"
+    },
+    {
+        quote: "What's my age again?",
+        author: "Blink-182"
+    },
+    {
+        quote: "Hand me my pants",
+        author: "Michael Grant, Hunger"
+    }
+]
